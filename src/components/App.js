f540fa7df5164d3370b26720a2ec2119e3c42b7b
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-09-17 21:30:46 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,40 @@ import fetchMock from '../fetch-setup'
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
         type: 'all'
       }
-    }
+    };
   }
 
-  fetchData(){
-    // fetchMock("/api/pets")
-    // .then(response=>response.json())
-    // .then(data=>{
-    //   console.log(data)
-    // })
-  }
+  fetchPets = () => {
+    let endpoint = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets }));
+  };
+
+  onChangeType = ({ target: { value } }) => {
+    this.setState({ filters: { ...this.state.filters, type: value } });
+  };
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets: pets });
+  };
+
   render() {
-    this.fetchData();
     return (
       <div className="ui container">
         <header>
@@ -33,17 +48,21 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
+
 
 export default App
 // <App />
